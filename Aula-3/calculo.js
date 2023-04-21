eace3df69ed409f1f61b4e6ffcd15ebcac514111
ThamiOliveira <thamioliveira6@gmail.com>
2023-04-20 22:01:55 -0300

    adicionando funções

--- Aula-3/calculo.js
+++ Aula-3/calculo.js
@@ -1,7 +1,17 @@
 let button = document.getElementById("calcular");
 
 button.addEventListener("click", calcular);
+button.addEventListener("mouseover", mudaCor);
+
+let elementoResultado = document.getElementById("resultado")
 
 function calcular(){
-    console.log("oi");
+    let nomeCachorro = document.getElementById("nome").value;
+
+    elementoResultado.innerHTML = `${nomeCachorro} deve comer 100g por dia`
+}
+
+function mudaCor(){
+    button.style.backgroundColor= "lightblue";
+
 }
